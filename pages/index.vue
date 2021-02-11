008a5fec66906ced9f5a5719fb7fa8279857afc4
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-02-11 13:52:15 +0330

    search and filter added

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <b-container>
+    <b-row cols="d-flex jutify-content-between align-items-center">
+      <b-col cols="5">
+        <b-input class="bg-dark border-dark shadow-lg mt-4" placeholder="search for a country..." />
+      </b-col>
+      <b-col cols="5" />
+      <b-col cols="2">
+        <b-form-select
+          v-model="selectedContinent"
+          class="mt-4 bg-dark border-dark shadow-lg text-white"
+          placeholder="Filter by Region"
+          :options="['africa','america','asia','europe','oceania']"
+        />
+      </b-col>
+    </b-row>
     <b-row class="mt-4">
       <b-col cols="3">
         <custom-card />
@@ -22,6 +36,11 @@ import CustomCard from '~/components/pages/home/CustomCard.vue'
 export default {
   components: {
     CustomCard
+  },
+  data () {
+    return {
+      selectedContinent: ''
+    }
   }
 }
 </script>
