35d0b600c7d119d7326993347e2a3747357c70f4
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-02-11 14:21:55 +0330

    create global backbutton component

--- pages/country/_slug.vue
+++ pages/country/_slug.vue
@@ -1,12 +1,19 @@
 <template>
   <b-container class="text-white">
-    country name
+    <b-row>
+      <b-col cols="1">
+        <back-button class="my-4" />
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
+import BackButton from '~/components/global/BackButton.vue'
 export default {
-
+  components: {
+    BackButton
+  }
 }
 </script>
 
