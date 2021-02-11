8db75ea4837ddb388792e48cae63dbe558b4c0f1
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-02-11 11:07:03 +0330

    install style reesources and sass loader and set config

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module', '@nuxtjs/style-resources'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -43,7 +43,12 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/dotenv'
   ],
-
+  styleResources: {
+    scss: [
+      '~/assets/scss/*.scss',
+      '~/assets/scss/app.scss'
+    ]
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
