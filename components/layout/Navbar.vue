ebf3427bc9adf859d9aa723e4c2e94161f6ff1a1
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-02-11 13:13:30 +0330

    create navbar

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="bg-dark">
-    header
+  <div class="bg-dark text-white shadow-lg">
+    <b-container fluid>
+      <b-row class="py-2 d-flex align-items-baseline">
+        <b-col cols="6" class="text-left">
+          <h3>
+            where is the world?
+          </h3>
+        </b-col>
+        <b-col cols="6" class="text-right">
+          <p>DarkMode</p>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
