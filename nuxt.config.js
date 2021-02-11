b28e4c292f5877ade9b4f3e1ea8459b4a38dbedc
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-02-11 10:22:38 +0330

    install vue-api-query for restApi model

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/vue-api-query'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
