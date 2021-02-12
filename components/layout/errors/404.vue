247a6b5992641cba317f90942d41f2551d353a56
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-02-12 16:37:14 +0330

    add errorpage handling component with sub components

--- components/layout/errors/404.vue
+++ components/layout/errors/404.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    <h1>404</h1>
-  </div>
+  <b-container>
+    <b-row>
+      <b-col class="d-flex text-center flex-column justify-content-center">
+        <h1 class="font-300">
+          404
+        </h1>
+        <h3>
+          {{ error.message }}
+        </h3>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {
-
+    props: {
+        error: {
+            type: Object,
+            required: true
+        }
+    }
 }
 </script>
 
--- components/layout/errors/500.vue
+++ components/layout/errors/500.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    <h1>404</h1>
-  </div>
+  <b-container>
+    <b-row>
+      <b-col class="d-flex text-center flex-column justify-content-center">
+        <h1 class="font-300">
+          500
+        </h1>
+        <h3>
+          {{ error.message }}
+        </h3>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
+}
 </script>
 
 <style></style>
