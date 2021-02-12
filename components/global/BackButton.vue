8a7de0767e43bc4206d7edd35affde3131847e86
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-02-12 10:57:16 +0330

    ionicons add and set icons for backbutton and navbar dark-mode

--- components/global/BackButton.vue
+++ components/global/BackButton.vue
@@ -1,15 +1,20 @@
 <template>
   <div
     :style="{backgroundColor : setBgColor , border : setBgColor}"
-    class="back-btn shadow-lg rounded  text-center px-5 py-1 cursor-pointer"
+    class="back-btn shadow-lg rounded d-flex justify-content-center  text-center px-5 py-1 cursor-pointer"
     @click="$router.back()"
   >
+    <MdArrowBackIcon class="pr-2" :style="{fill : setTextColor}"/>
     Back
   </div>
 </template>
 
 <script>
+import MdArrowBackIcon from 'vue-ionicons/dist/md-arrow-back.vue'
 export default {
+  components: {
+    MdArrowBackIcon
+  },
   computed: {
     setBgColor () {
       return this.$nuxt.$colorMode.preference === 'dark' ? 'hsl(209, 23%, 22%)' : 'hsl(0, 0%, 100%)'
