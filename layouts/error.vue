631252dd8f74c77ff8339d222d7bb5a01703c69b
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-02-12 16:24:24 +0330

    add errorpage handling component with sub components

---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <component :is="errorPage" :error="error" />
+  </div>
+</template>
+
+<script>
+import error404 from '~/components/layout/errors/404.vue'
+import error500 from '~/components/layout/errors/500.vue'
+export default {
+    name: 'NuxtError',
+    layout: 'default',
+    props: {
+        error: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    computed: {
+        errorPage () {
+            switch (this.error.statusCode) {
+                case 404 :
+                    return error404
+                case 500 :
+                    return error500
+            }
+            return this.error.statusCode
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
